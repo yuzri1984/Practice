c61b0d0c4b64f7f2cd357dd91961820bb949cb49
yuzri1984 <yuzrifitriah@hotmail.com>
2022-06-22 01:35:56 +0800

    Practice Push

---
+++ myapp/core/Name.java
@@ -0,0 +1,26 @@
+package myapp.core;
+
+
+import java.io.Console;
+
+
+public class Name {
+public static void main(String[] args){
+
+Console cons = System.console();
+String name = cons.readLine("What is your name?");
+if (name.length() <= 0) {
+System.err.println(" Name not entered.");
+
+
+}
+else 
+
+System.out.printf("Hello %s. Please to meet you..\n", name);
+
+
+}    
+
+
+
+}
